e35d97c09ff0219765fed335cbf328be74e9952d
Roger Foss <rogerfsiqueira@gmail.com>
2023-03-09 09:49:28 -0300

    ajustando SessaoDeVotacao Testes

--- votacao/src/test/java/br/tec/db/votacao/controller/SessaoDeVotacaoControllerTest.java
+++ votacao/src/test/java/br/tec/db/votacao/controller/SessaoDeVotacaoControllerTest.java
@@ -1,11 +1,19 @@
 package br.tec.db.votacao.controller;
 
-import br.tec.db.votacao.dto.SessaoDeVotacaoDTO;
+import br.tec.db.votacao.dto.sessaoDeVotacaoDTO.BuscarSessaoDeVotacaoDTO;
+import br.tec.db.votacao.dto.sessaoDeVotacaoDTO.CriarSessaoDeVotacaoDTO;
+import br.tec.db.votacao.enums.SessaoDeVotacaoStatusEnum;
+import br.tec.db.votacao.mapper.SessaoDeVotacaoMapper;
+import br.tec.db.votacao.model.Pauta;
+import br.tec.db.votacao.model.SessaoDeVotacao;
 import br.tec.db.votacao.service.SessaoDeVotacaoService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,77 +30,99 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 public class SessaoDeVotacaoControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private JacksonTester<CriarSessaoDeVotacaoDTO> criarSessaoDeVotacaoDtoJson;
+
     @MockBean
     private SessaoDeVotacaoService sessaoDeVotacaoService;
 
+    private List<BuscarSessaoDeVotacaoDTO> sessoesDeVotacao;
+    private SessaoDeVotacao sessaoDeVotacao1, sessaoDeVotacao2;
+
+    @BeforeEach
+    public void inicializar() {
+        sessaoDeVotacao1 = new SessaoDeVotacao(1L, LocalDateTime.now(), null, SessaoDeVotacaoStatusEnum.INICIADA, new Pauta(), null);
+        sessaoDeVotacao2 = new SessaoDeVotacao(2L, LocalDateTime.now(), null, SessaoDeVotacaoStatusEnum.INICIADA, new Pauta(), null);
+
+        sessoesDeVotacao = new ArrayList<>();
+        sessoesDeVotacao.add(new BuscarSessaoDeVotacaoDTO(sessaoDeVotacao1));
+        sessoesDeVotacao.add(new BuscarSessaoDeVotacaoDTO(sessaoDeVotacao2));
+    }
+
     @Test
     public void deveCriarUmaSessaoDeVotacaoEmUmaPauta() throws Exception {
-        SessaoDeVotacaoDTO sessaoDeVotacaoDTO = new SessaoDeVotacaoDTO(LocalDateTime.now(), 1L);
-        when(sessaoDeVotacaoService.criarSessaoDeVotacao(any(SessaoDeVotacaoDTO.class))).thenReturn(sessaoDeVotacaoDTO);
+        CriarSessaoDeVotacaoDTO criarSessaoDeVotacaoDTO = new CriarSessaoDeVotacaoDTO(LocalDateTime.now(), 1L);
+        when(sessaoDeVotacaoService.criarSessaoDeVotacao(criarSessaoDeVotacaoDTO)).thenReturn(SessaoDeVotacaoMapper.buildSessaoDeVotacao(criarSessaoDeVotacaoDTO));
+
 
         mockMvc.perform(post("/sessao-de-votacao")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"inicio\": \"2023-06-01T10:00:00\", \"idPauta\": \"1\"}"))
+                        .content(criarSessaoDeVotacaoDtoJson.write(criarSessaoDeVotacaoDTO).getJson()))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
     }
 
     @Test
     public void deveRetornarBadRequestAoCriarSessaoDeVotacaoComDTONull() throws Exception {
-        when(sessaoDeVotacaoService.criarSessaoDeVotacao(any(SessaoDeVotacaoDTO.class))).thenReturn(null);
+        when(sessaoDeVotacaoService.criarSessaoDeVotacao(any(CriarSessaoDeVotacaoDTO.class))).thenReturn(null);
 
         mockMvc.perform(post("/sessao-de-votacao")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"inicio\": \"null\", \"idPauta\": \"null\"}"))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void deveRetornarBadRequestAoCriarSessaoEmUmaPautaJaEncerrada() throws Exception {
+        when(sessaoDeVotacaoService.criarSessaoDeVotacao(any(CriarSessaoDeVotacaoDTO.class))).thenThrow(new RuntimeException("Pauta já encerrada!"));
+
+        mockMvc.perform(post("/sessao-de-votacao")
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     public void deveBuscarTodasAsSessoesDeVotacao() throws Exception {
-        List<SessaoDeVotacaoDTO> sessaoDeVotacaoDTO = new ArrayList<>();
-        sessaoDeVotacaoDTO.add(new SessaoDeVotacaoDTO(LocalDateTime.now(), 1L));
-        sessaoDeVotacaoDTO.add(new SessaoDeVotacaoDTO(LocalDateTime.now(), 2L));
-        sessaoDeVotacaoDTO.add(new SessaoDeVotacaoDTO(LocalDateTime.now(), 3L));
-        when(sessaoDeVotacaoService.buscarTodasAsSessoesDeVotacao()).thenReturn(sessaoDeVotacaoDTO);
+
+        when(sessaoDeVotacaoService.buscarTodasAsSessoesDeVotacao()).thenReturn(sessoesDeVotacao);
 
         mockMvc.perform(get("/sessao-de-votacao")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(3)));
+                .andExpect(jsonPath("$", hasSize(2)));
     }
 
     @Test
     public void deveRetornarNotFoundAoBuscarTodasAsSessoesDeVotacaoVazio() throws Exception {
-        List<SessaoDeVotacaoDTO> sessaoDeVotacaoDTO = new ArrayList<>();
-        when(sessaoDeVotacaoService.buscarTodasAsSessoesDeVotacao()).thenReturn(sessaoDeVotacaoDTO);
+        List<BuscarSessaoDeVotacaoDTO> sessoesDeVotacao = new ArrayList<>();
+        when(sessaoDeVotacaoService.buscarTodasAsSessoesDeVotacao()).thenReturn(sessoesDeVotacao);
 
-        mockMvc.perform(get("/sessao-de-votacao")
-                        .contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/sessao-de-votacao"))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     public void deveBuscarSessaoDeVotacaoPorId() throws Exception {
-        SessaoDeVotacaoDTO sessaoDeVotacaoDTO = new SessaoDeVotacaoDTO(LocalDateTime.now(), 3L);
-        when(sessaoDeVotacaoService.buscarSessaoDeVotacaoPorId(1L)).thenReturn(sessaoDeVotacaoDTO);
+
+        when(sessaoDeVotacaoService.buscarSessaoDeVotacaoPorId(1L)).thenReturn(new BuscarSessaoDeVotacaoDTO(sessaoDeVotacao1));
 
         mockMvc.perform(get("/sessao-de-votacao/1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L));
     }
 
     @Test
     public void deveRetornarNotFoundAoBuscarSessaoDeVotacaoPorIdInexistente() throws Exception {
 
-        mockMvc.perform(get("/sessao-de-votacao/10")
-                        .contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/sessao-de-votacao/10"))
                 .andExpect(status().isNotFound());
     }
 
@@ -106,13 +136,14 @@ public class SessaoDeVotacaoControllerTest {
 
     @Test
     public void deveBuscarSessaoDeVotacaoPorPauta() throws Exception {
-        SessaoDeVotacaoDTO sessaoDeVotacaoDTO = new SessaoDeVotacaoDTO(LocalDateTime.now(), 1L);
-        when(sessaoDeVotacaoService.buscarSessaoDeVotacaoPorPauta(1L)).thenReturn(sessaoDeVotacaoDTO);
+
+        when(sessaoDeVotacaoService.buscarSessaoDeVotacaoPorPauta(1L)).thenReturn(new BuscarSessaoDeVotacaoDTO(sessaoDeVotacao1));
 
         mockMvc.perform(get("/sessao-de-votacao/pauta/1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L));
     }
 
     @Test
@@ -133,9 +164,6 @@ public class SessaoDeVotacaoControllerTest {
 
     @Test
     public void deveFinalizarUmaSessaoDeVotacao() throws Exception {
-        List<SessaoDeVotacaoDTO> sessao = new ArrayList<>();
-        sessao.add(new SessaoDeVotacaoDTO(LocalDateTime.now(), 1L));
-        when(sessaoDeVotacaoService.buscarTodasAsSessoesDeVotacao()).thenReturn(sessao);
 
         mockMvc.perform(put("/sessao-de-votacao/encerrar/1")
                         .contentType(MediaType.APPLICATION_JSON))
@@ -144,9 +172,6 @@ public class SessaoDeVotacaoControllerTest {
 
     @Test
     public void deveCalcularResultadoDaSessaoDeVotacao() throws Exception {
-        List<SessaoDeVotacaoDTO> sessao = new ArrayList<>();
-        sessao.add(new SessaoDeVotacaoDTO(LocalDateTime.now(), 1L));
-        when(sessaoDeVotacaoService.buscarTodasAsSessoesDeVotacao()).thenReturn(sessao);
 
         mockMvc.perform(put("/sessao-de-votacao/resultado/1")
                         .contentType(MediaType.APPLICATION_JSON))
--- votacao/src/test/java/br/tec/db/votacao/service/SessaoDeVotacaoServiceImplTest.java
+++ votacao/src/test/java/br/tec/db/votacao/service/SessaoDeVotacaoServiceImplTest.java
@@ -1,10 +1,11 @@
 package br.tec.db.votacao.service;
 
-import br.tec.db.votacao.dto.SessaoDeVotacaoDTO;
+import br.tec.db.votacao.dto.sessaoDeVotacaoDTO.CriarSessaoDeVotacaoDTO;
 import br.tec.db.votacao.enums.PautaStatusEnum;
 import br.tec.db.votacao.enums.SessaoDeVotacaoStatusEnum;
 import br.tec.db.votacao.model.Pauta;
 import br.tec.db.votacao.model.SessaoDeVotacao;
+import br.tec.db.votacao.repository.PautaRepository;
 import br.tec.db.votacao.repository.SessaoDeVotacaoRepository;
 import br.tec.db.votacao.service.impl.SessaoDeVotacaoServiceImpl;
 import org.junit.jupiter.api.Disabled;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
@@ -26,23 +28,27 @@ class SessaoDeVotacaoServiceImplTest {
     @Mock
     private SessaoDeVotacaoRepository sessaoDeVotacaoRepository;
 
+    @Mock
+    private PautaRepository pautaRepository;
+
+    @Mock
+    private PautaService pautaService;
+
     @InjectMocks
     private SessaoDeVotacaoServiceImpl sessaoDeVotacaoService;
 
     @Disabled
     @Test
-    void deveCriarSessaoDeVotacao() {
-        Pauta pauta = new Pauta();
-        pauta.setStatus(PautaStatusEnum.AGUARDANDO_VOTACAO);
-        SessaoDeVotacao sessaoDeVotacao = new SessaoDeVotacao();
-        sessaoDeVotacao.setStatus(SessaoDeVotacaoStatusEnum.INICIADA);
-        sessaoDeVotacao.setPauta(pauta);
-        sessaoDeVotacao.setInicio(LocalDateTime.now());
-        sessaoDeVotacao.setFim(LocalDateTime.now().plusMinutes(1));
-        when(sessaoDeVotacaoRepository.save(sessaoDeVotacao)).thenReturn(sessaoDeVotacao);
-        SessaoDeVotacaoDTO sessaoDeVotacaoDTO = sessaoDeVotacaoService.criarSessaoDeVotacao(new SessaoDeVotacaoDTO(sessaoDeVotacao));
-        assertNotNull(sessaoDeVotacaoDTO);
-        assertEquals(sessaoDeVotacao.getStatus(), SessaoDeVotacaoStatusEnum.INICIADA);
+    public void criarSessaoDeVotacaoTest() {
+        CriarSessaoDeVotacaoDTO criarSessaoDeVotacaoDTO = new CriarSessaoDeVotacaoDTO(LocalDateTime.now(), 1L);
+        Pauta pauta = new Pauta(1L, "Pauta 1", PautaStatusEnum.AGUARDANDO_VOTACAO, null, null);
+
+        when(this.sessaoDeVotacaoRepository.save(Mockito.any(SessaoDeVotacao.class))).thenReturn(new SessaoDeVotacao(1L, LocalDateTime.now(), null, SessaoDeVotacaoStatusEnum.INICIADA, pauta, null));
+
+        SessaoDeVotacao sessaoDeVotacao = sessaoDeVotacaoService.criarSessaoDeVotacao(criarSessaoDeVotacaoDTO);
+
+        assertNotNull(sessaoDeVotacao);
+        assertEquals(SessaoDeVotacaoStatusEnum.INICIADA, sessaoDeVotacao.getStatus());
     }
 
 }
